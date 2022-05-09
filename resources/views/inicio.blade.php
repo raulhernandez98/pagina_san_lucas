09efc91805091ee7d52681889b467ace0d1b5727
raulhernandez98 <rhernandezbass@gmail.com>
2022-05-08 21:28:39 -0600

    Avance 1

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>San Lucas Medical Center</title>
+	<link rel="stylesheet" href="estilos.css">
+	<link rel="stylesheet" href="fonts.css">
+</head>
+
+<body>
+    <header>
+		<nav>
+			<ul>
+				<li><a href="#"><span class=""><i class="icon icon-users"></i></span>¿Quiénes somos?</a></li>
+				<li><a href="#"><span class=""><i class="icon icon-briefcase"></i></span>Servicios</a></li>
+					<ul>
+						<li><a href="#">Laboratorio</a></li>
+						<li><a href="#">Clínicas</a></li>
+						<li><a href="#">Farmacia</a></li>
+					</ul>
+				<li><a href="#"><span class=""><i class="icon icon-location2"></i></span>Instalaciones</a></li>
+				<li><a href="#"><span class=""><i class="icon icon-clipboard"></i></span>Instituciones asociadas</a></li>
+				<li><a href="#"><span class=""><i class="fa-solid fa-phone"></i></i></span>Contactos</a></li>
+			</ul>
+		</nav>
+	</header>
+    
+</body>
+
+</html>
